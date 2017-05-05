83d4b1d157afd5b2351343ffe069ba30714eea18
hitesh.mistry <hsmistry@users.noreply.github.com>
2017-05-05 16:32:15 +0100

    Added pl1000 driver basic console application

---
+++ pl1000/PL1000CSConsole/PL1000CSConsole.cs
@@ -0,0 +1,138 @@
+﻿/*******************************************************************************
+ *
+ * Filename: PL1000CSConsole.cs
+ *
+ * Description:
+ *   This is a console-mode program that demonstrates how to use the
+ *   pl1000 driver using .NET
+ *
+ * Supported PicoLog models:
+ *
+ *		PicoLog 1012 & 1216
+ *		
+ * Examples:
+ *    Open a connection and display unit information before closing the 
+ *    connection to the device.
+ *    
+ * Copyright (C) 2012 - 2017 Pico Technology Ltd. See LICENSE file for terms.    
+ *    
+ *******************************************************************************/
+
+using System;
+using System.Threading;
+
+using PL1000Imports;
+
+namespace PL1000CSConsole
+{
+    class PL1000CSConsole
+    {
+        private short _handle;
+
+        /* ********************************************************************************************************************************
+        *  WaitForKey() 
+        * 
+        *  Wait for user keypress
+        *  ********************************************************************************************************************************/
+
+        private static void WaitForKey()
+        {
+            while (!Console.KeyAvailable) Thread.Sleep(100);
+            if (Console.KeyAvailable) Console.ReadKey(true); // clear the key
+        }
+
+        /***********************************************************************************************
+         * GetDeviceInfo()
+         * Show information about device
+         * 
+        **********************************************************************************************/
+        void GetDeviceInfo()
+        {
+            string[] description = {
+                                       "Driver Version    ",
+                                       "USB Version       ",
+                                       "Hardware Version  ",
+                                       "Variant Info      ",
+                                       "Batch and Serial  ", 
+						               "Calibration Date  ", 
+						               "Kernel Driver Ver "
+                                    };
+            
+            System.Text.StringBuilder line = new System.Text.StringBuilder(80);
+
+            if (_handle >= 0)
+            {
+
+                for (int i = 0; i < 7; i++)
+                {
+                    short requiredSize;
+                    Imports.GetUnitInfo(_handle, line, 80, out requiredSize, i);
+
+                    Console.WriteLine("{0}: {1}", description[i], line);
+                }
+            }
+        }
+
+
+        /***********************************************************************************************
+        * Run()
+        * Show information about device
+        * 
+        **********************************************************************************************/
+        public void Run()
+        {
+            // Display unit information
+            GetDeviceInfo();
+        }
+
+        /***********************************************************************************************
+        * PL1000ConsoleExample()
+        * Show information about device
+        * 
+        **********************************************************************************************/
+        PL1000CSConsole(short handle)
+        {
+            _handle = handle;
+        }
+
+        static void Main(string[] args)
+        {
+            short handle;
+
+            System.Text.StringBuilder str = new System.Text.StringBuilder(80);
+            str = null;
+
+            Console.WriteLine("PicoLog 1000 Series (pl1000) Driver Example Program");
+            Console.WriteLine("Version 1.1\n");
+
+            // Open unit 
+            Console.WriteLine("\nAttempting to open a device...");
+
+            Imports.OpenUnit(out handle);
+
+            Console.WriteLine("Handle: {0}", handle);
+
+            if (handle == 0)
+            {
+                Console.WriteLine("Unable to open device");
+                Console.WriteLine("Error code : {0}", handle);
+            }
+            else
+            {
+                Console.WriteLine("Device opened successfully:\n");
+
+                PL1000CSConsole consoleExample = new PL1000CSConsole(handle);
+                consoleExample.Run();
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit the application.");
+                WaitForKey();
+
+                Imports.CloseUnit(handle);
+            }
+
+            
+
+        }
+    }
+}
---
+++ pl1000/PL1000Imports.cs
@@ -0,0 +1,160 @@
+﻿/*******************************************************************************
+ *
+ * Filename: PL1000Imports.cs
+ *
+ * Description:
+ *      This file his file contains .NET wrapper calls for the pl1000 driver 
+ *      API functions. It also has the enums required by the (wrapped) function 
+ *      calls.
+ *		
+ * Examples:
+ *    Open a connection and display unit information before closing the 
+ *    connection to the device.
+ *    
+ * Copyright (C) 2012 - 2017 Pico Technology Ltd. See LICENSE file for terms.    
+ *    
+ *******************************************************************************/
+
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PL1000Imports
+{
+    class Imports
+    {
+        #region constants
+        private const string _DRIVER_FILENAME = "pl1000.dll";
+
+
+        #endregion
+
+        #region Driver enums
+
+        public enum enPL1000Inputs
+        {
+            PL1000_CHANNEL_1 = 1,
+            PL1000_CHANNEL_2,
+            PL1000_CHANNEL_3,
+            PL1000_CHANNEL_4,
+            PL1000_CHANNEL_5,
+            PL1000_CHANNEL_6,
+            PL1000_CHANNEL_7,
+            PL1000_CHANNEL_8,
+            PL1000_CHANNEL_9,
+            PL1000_CHANNEL_10,
+            PL1000_CHANNEL_11,
+            PL1000_CHANNEL_12,
+            PL1000_CHANNEL_13,
+            PL1000_CHANNEL_14,
+            PL1000_CHANNEL_15,
+            PL1000_CHANNEL_16,
+            PL1000_MAX_CHANNELS = PL1000_CHANNEL_16
+        }
+
+        public enum enPL1000DO_Channel
+        {
+            PL1000_DO_CHANNEL_0,
+            PL1000_DO_CHANNEL_1,
+            PL1000_DO_CHANNEL_2,
+            PL1000_DO_CHANNEL_3,
+            PL1000_DO_CHANNEL_MAX
+        }
+
+        public enum enPL1000OpenProgress
+        {
+            PL1000_OPEN_PROGRESS_FAIL = -1,
+            PL1000_OPEN_PROGRESS_PENDING = 0,
+            PL1000_OPEN_PROGRESS_COMPLETE = 1
+        }
+
+        public enum enPL1000Method
+        {
+            SINGLE,
+            WINDOW,
+            STREAM,
+            MAX_METHOD = STREAM
+        }
+
+        #endregion
+
+        
+        #region Driver Imports
+
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000CloseUnit")]
+        public static extern short CloseUnit(short handle);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetUnitInfo")]
+        public static extern short GetUnitInfo(short handle,
+                                              StringBuilder infoString,
+                                              short strlength,
+                                              out short reqSize,
+                                              int info);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnit")]
+        public static extern short OpenUnit(out short handle);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetSingle")]
+        public static extern short Enumerate(short handle,
+                                              enPL1000Inputs channel,
+                                              out ushort value);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetValues")]
+        public static extern short GetValue(short handle,
+                                            short[] value,
+                                            ref ushort noOfValues,
+                                            out ushort overflow,
+                                            out uint triggerIndex);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000MaxValue")]
+        public static extern short MaxValue(short handle,
+                                            out ushort maxvalue);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnitAsync")]
+        public static extern short OpenUnitAsync(out short handle);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnitProgress")]
+        public static extern short pl1000OpenUnitProgress(out short handle,
+                                                            out short progress,
+                                                            out short complete);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Ready")]
+        public static extern short pl1000Ready(short handle,
+                                                out short ready);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Run")]
+        public static extern short Run(short handle,
+                                        uint NoOfValues,
+                                        enPL1000Method method);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetDo")]
+        public static extern short SetDo(short handle,
+                                            short do_value,
+                                            short DOchannel);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetInterval")]
+        public static extern short SetInterval(short handle,
+                                                ref uint usforblock,
+                                                uint IdealNoOfSamples,
+                                                short[] channels,
+                                                short NoofChannels);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetTrigger")]
+        public static extern short SetTrigger(short handle,
+                                              ushort enabled,
+                                              ushort autoTrigger,
+                                              ushort autoMS,
+                                              enPL1000Inputs channel,
+                                              ushort dir,
+                                              ushort threshold,
+                                              ushort hysteresis,
+                                              float delay);
+
+        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Stop")]
+        public static extern short stop(short handle);
+
+        #endregion
+    }
+
+
+}
