89859f7a63f77be1ff76669a93b15ae8bc1c7f6f
mark.pidcock <mark.pidcock@picotech.com>
2017-11-02 08:47:30 +0000

    Update Project properties

--- usbdrdaq/USBDRDAQRemote/Properties/AssemblyInfo.cs
+++ usbdrdaq/USBDRDAQRemote/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("USBDRDAQRemote")]
-[assembly: AssemblyDescription("Remote Server Application")]
+[assembly: AssemblyDescription("USB DrDAQ Remote Server C# Example Program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pico Technology Ltd.")]
 [assembly: AssemblyProduct("USB DrDAQ Data Logger")]
-[assembly: AssemblyCopyright("Copyright © Pico Technology Ltd 2012")]
+[assembly: AssemblyCopyright("Copyright © Pico Technology Ltd 2012-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
