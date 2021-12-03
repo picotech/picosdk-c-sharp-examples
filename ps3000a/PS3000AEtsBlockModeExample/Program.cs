5d4c9a9b2b1c61a6368c2de085dda4c936a81480
cedric.carteron <cedric.carteron@picotech.com>
2021-12-03 17:16:52 +0000

    Initial Commit

---
+++ ps3000a/PS3000AEtsBlockModeExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+using PS3000AImports;
+using PicoPinnedArray;
+using PicoStatus;
+
+namespace PS3000AEtsBlockModeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
