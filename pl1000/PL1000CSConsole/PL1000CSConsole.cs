9c83efcdb6cd131d11611ef6b8f752b538e5fe9b
Alex Dunn <alexander.dunn@picotech.com>
2024-05-20 14:59:31 +0100

    Implement streamed capturing and averaging of channel voltage readings

--- pl1000/PL1000CSConsole/PL1000CSConsole.cs
+++ pl1000/PL1000CSConsole/PL1000CSConsole.cs
@@ -20,119 +20,215 @@
 
 using System;
 using System.Threading;
+using System.Linq;
 
 using PL1000Imports;
+using PicoErrorCode = PL1000Imports.Imports.PicoErrorCode;
+using System.Collections.Generic;
 
 namespace PL1000CSConsole
 {
-    class PL1000CSConsole
+  class PL1000CSConsole
+  {
+    private short _handle;
+    private ushort _maxADCValue;
+
+    /* ********************************************************************************************************************************
+    *  WaitForKey() 
+    * 
+    *  Wait for user keypress
+    *  ********************************************************************************************************************************/
+
+    private static void WaitForKey()
     {
-        private short _handle;
-
-        /* ********************************************************************************************************************************
-        *  WaitForKey() 
-        * 
-        *  Wait for user keypress
-        *  ********************************************************************************************************************************/
-
-        private static void WaitForKey()
-        {
-            while (!Console.KeyAvailable) Thread.Sleep(100);
-            if (Console.KeyAvailable) Console.ReadKey(true); // clear the key
-        }
-
-        /***********************************************************************************************
-         * GetDeviceInfo()
-         * Show information about device
-         * 
-        **********************************************************************************************/
-        void GetDeviceInfo()
-        {
-            string[] description = {
-                                       "Driver Version    ",
-                                       "USB Version       ",
-                                       "Hardware Version  ",
-                                       "Variant Info      ",
-                                       "Batch and Serial  ", 
-						               "Calibration Date  ", 
-						               "Kernel Driver Ver "
-                                    };
-            
-            System.Text.StringBuilder line = new System.Text.StringBuilder(80);
-
-            if (_handle >= 0)
-            {
-
-                for (int i = 0; i < 7; i++)
-                {
-                    short requiredSize;
-                    Imports.GetUnitInfo(_handle, line, 80, out requiredSize, i);
-
-                    Console.WriteLine("{0}: {1}", description[i], line);
-                }
-            }
-        }
-
-
-        /***********************************************************************************************
-        * Run()
-        * Show information about device
-        * 
-        **********************************************************************************************/
-        public void Run()
-        {
-            // Display unit information
-            GetDeviceInfo();
-        }
-
-        /***********************************************************************************************
-        * PL1000ConsoleExample()
-        * Show information about device
-        * 
-        **********************************************************************************************/
-        PL1000CSConsole(short handle)
-        {
-            _handle = handle;
-        }
-
-        static void Main(string[] args)
-        {
-            short handle;
-
-            System.Text.StringBuilder str = new System.Text.StringBuilder(80);
-            str = null;
-
-            Console.WriteLine("PicoLog 1000 Series (pl1000) Driver Example Program");
-            Console.WriteLine("Version 1.1\n");
-
-            // Open unit 
-            Console.WriteLine("\nAttempting to open a device...");
-
-            Imports.OpenUnit(out handle);
-
-            Console.WriteLine("Handle: {0}", handle);
-
-            if (handle == 0)
-            {
-                Console.WriteLine("Unable to open device");
-                Console.WriteLine("Error code : {0}", handle);
-            }
-            else
-            {
-                Console.WriteLine("Device opened successfully:\n");
-
-                PL1000CSConsole consoleExample = new PL1000CSConsole(handle);
-                consoleExample.Run();
-
-                Console.WriteLine();
-                Console.WriteLine("Press any key to exit the application.");
-                WaitForKey();
-
-                Imports.CloseUnit(handle);
-            }
-
-            
-
-        }
+      while (!Console.KeyAvailable) Thread.Sleep(100);
+      if (Console.KeyAvailable) Console.ReadKey(true); // clear the key
     }
+
+    /***********************************************************************************************
+     * GetDeviceInfo()
+     * Show information about device
+     * 
+    **********************************************************************************************/
+    void GetDeviceInfo()
+    {
+      string[] description =
+        {
+          "Driver Version    ",
+          "USB Version       ",
+          "Hardware Version  ",
+          "Variant Info      ",
+          "Batch and Serial  ",
+          "Calibration Date  ",
+          "Kernel Driver Ver "
+        };
+
+      System.Text.StringBuilder line = new System.Text.StringBuilder(80);
+
+      if (_handle >= 0)
+      {
+        for (int i = 0; i < 7; i++)
+        {
+          short requiredSize;
+          Imports.GetUnitInfo(_handle, line, 80, out requiredSize, i);
+          Console.WriteLine("{0}: {1}", description[i], line);
+        }
+      }
+    }
+
+
+    /***********************************************************************************************
+    * Run()
+    * Capture streamed data for a given number of channels and display the averaged voltages
+    * 
+    **********************************************************************************************/
+    public void Run(short noOfChannels)
+    {
+      PicoErrorCode errorCode = 0;
+
+      // setup the p1000 device to sample on all channels at 1kS/s
+      const ushort noOfSamplesPerChannel = 1000;
+      Imports.enPL1000Method captureMode = Imports.enPL1000Method.STREAM;
+      ushort totalSamples = (ushort)(noOfSamplesPerChannel * noOfChannels);
+      uint usForBlock = 1000000; // 1s
+
+      Console.WriteLine($"\nTest on {noOfChannels} channels...");
+      Console.WriteLine("----------------------------------");
+
+      List<short> channels = new List<short>();
+      for (short i = 1; i <= noOfChannels; i++)
+        channels.Add(i);
+
+      Console.WriteLine("\nSet the sampling interval on device...");
+      errorCode = Imports.SetInterval(_handle, ref usForBlock, noOfSamplesPerChannel, channels.ToArray(), noOfChannels);
+      CheckDriverErrorCode(errorCode);
+      // write out to console the usForBlock, noOfSamplesPerChannel and noOfChannels
+      Console.WriteLine($"\nusForBlock: {usForBlock}\nnoOfSamplesPerChannel: {noOfSamplesPerChannel}\nnoOfChannels: {noOfChannels}");
+
+      // capture data from the device using the run method
+      Console.WriteLine($"\nStart capturing on device...");
+      errorCode = Imports.Run(_handle, noOfSamplesPerChannel, captureMode);
+      CheckDriverErrorCode(errorCode);
+
+      const int msSleepTime = 2000;
+      Thread.Sleep(msSleepTime);
+
+      // Call GetValue method
+      ushort overflow = 0;
+      ushort[] values = new ushort[noOfSamplesPerChannel * noOfChannels];
+      uint numberOfSamples = noOfSamplesPerChannel;
+      uint triggerIndex = 0; // the returned value can be ignored as we're capturing with triggering disabled
+
+      Console.WriteLine("\nGather data from device...");
+      errorCode = Imports.GetValue(_handle, values, ref numberOfSamples, out overflow, out triggerIndex);
+      CheckDriverErrorCode(errorCode);
+      Console.WriteLine($"\n{numberOfSamples} samples per channel were captured over {msSleepTime}ms");
+
+      if (numberOfSamples > 0)
+      {
+        // Average the samples per channel back from the device
+        ushort[][] channelValues = new ushort[noOfChannels][];
+        double[] averageVoltage = new double[noOfChannels];
+        for (int i = 0; i < noOfChannels; i++)
+        {
+          channelValues[i] = new ushort[numberOfSamples];
+
+          int index = 0;
+          for (int j = i; j < (numberOfSamples * noOfChannels); j += noOfChannels)
+          {
+            channelValues[i][index++] = values[j];
+          }
+
+          averageVoltage[i] = channelValues[i].Average(e => e * 2.5f / _maxADCValue);
+
+          // write the average voltage (2dp precision) to the console
+          Console.WriteLine($"Channel {i + 1} average voltage: {averageVoltage[i].ToString("F2")}V");
+        }
+      }
+    }
+
+    private static void CheckDriverErrorCode(PicoErrorCode errorCode)
+    {
+      if (errorCode != PicoErrorCode.PICO_OK)
+        throw new PicoErrorCodeException(errorCode);
+
+      Console.WriteLine("Success");
+    }
+
+    /***********************************************************************************************
+    * PL1000ConsoleExample()
+    * Show information about device
+    * 
+    **********************************************************************************************/
+    PL1000CSConsole(short handle)
+    {
+      _handle = handle;
+    }
+
+    static void Main(string[] args)
+    {
+      const short noOfChannels = 1;
+      short handle = -1;
+      PicoErrorCode errorCode = PicoErrorCode.PICO_OK;
+
+      Console.WriteLine("PicoLog 1000 Series (pl1000) Driver Example Program");
+      Console.WriteLine("Version 1.1\n");
+      try
+      {
+        // Open unit 
+        Console.WriteLine("\nAttempting to open a device...");
+        errorCode = Imports.OpenUnit(out handle);
+        CheckDriverErrorCode(errorCode);
+
+        // we only get here if the device opened successfully
+        Console.WriteLine("Handle: {0}", handle);
+        Console.WriteLine("Device opened successfully:\n");
+
+        PL1000CSConsole consoleExample = new PL1000CSConsole(handle);
+
+        // Display unit information
+        consoleExample.GetDeviceInfo();
+
+        // disable the trigger on the pl1000 device
+        consoleExample.SetTrigger();
+
+        // get the max adc value
+        consoleExample.GetMaxAdcValue();
+
+        for (short i = 1; i <= 16; i++)
+          consoleExample.Run(i);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error found: {ex.Message}");
+      }
+      finally
+      {
+        WaitForUserToContinue();
+
+        if (handle > 0)
+          Imports.CloseUnit(handle);
+      }
+    }
+
+    private void GetMaxAdcValue()
+    {
+      // get the maximum ADC value
+      Console.WriteLine("\nGetting the maximum ADC value from device...");
+      CheckDriverErrorCode(Imports.MaxValue(_handle, out _maxADCValue));
+    }
+
+    private void SetTrigger()
+    {
+      Console.WriteLine("\nDisable trigger on device...");
+      CheckDriverErrorCode(Imports.SetTrigger(_handle, 0, 0, 0, 0, 0, 0, 0, 0));
+    }
+
+    private static void WaitForUserToContinue()
+    {
+      Console.WriteLine();
+      Console.WriteLine("Press any key to exit the application.");
+      WaitForKey();
+    }
+  }
 }
--- pl1000/PL1000Imports.cs
+++ pl1000/PL1000Imports.cs
@@ -16,137 +16,192 @@ using System.Text;
 
 namespace PL1000Imports
 {
-    class Imports
+  class Imports
+  {
+    #region Constants
+    private const string _DRIVER_FILENAME = "pl1000.dll";
+
+    #endregion
+
+    #region Driver Enums
+
+    public enum PicoErrorCode
     {
-        #region Constants
-        private const string _DRIVER_FILENAME = "pl1000.dll";
-
-        #endregion
-
-        #region Driver Enums
-
-        public enum enPL1000Inputs
-        {
-            PL1000_CHANNEL_1 = 1,
-            PL1000_CHANNEL_2,
-            PL1000_CHANNEL_3,
-            PL1000_CHANNEL_4,
-            PL1000_CHANNEL_5,
-            PL1000_CHANNEL_6,
-            PL1000_CHANNEL_7,
-            PL1000_CHANNEL_8,
-            PL1000_CHANNEL_9,
-            PL1000_CHANNEL_10,
-            PL1000_CHANNEL_11,
-            PL1000_CHANNEL_12,
-            PL1000_CHANNEL_13,
-            PL1000_CHANNEL_14,
-            PL1000_CHANNEL_15,
-            PL1000_CHANNEL_16,
-            PL1000_MAX_CHANNELS = PL1000_CHANNEL_16
-        }
-
-        public enum enPL1000DO_Channel
-        {
-            PL1000_DO_CHANNEL_0,
-            PL1000_DO_CHANNEL_1,
-            PL1000_DO_CHANNEL_2,
-            PL1000_DO_CHANNEL_3,
-            PL1000_DO_CHANNEL_MAX
-        }
-
-        public enum enPL1000OpenProgress
-        {
-            PL1000_OPEN_PROGRESS_FAIL = -1,
-            PL1000_OPEN_PROGRESS_PENDING = 0,
-            PL1000_OPEN_PROGRESS_COMPLETE = 1
-        }
-
-        public enum enPL1000Method
-        {
-            SINGLE,
-            WINDOW,
-            STREAM,
-            MAX_METHOD = STREAM
-        }
-
-        #endregion
-        
-        #region Driver Imports
-
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000CloseUnit")]
-        public static extern short CloseUnit(short handle);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetUnitInfo")]
-        public static extern short GetUnitInfo(short handle,
-                                              StringBuilder infoString,
-                                              short strlength,
-                                              out short reqSize,
-                                              int info);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnit")]
-        public static extern short OpenUnit(out short handle);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetSingle")]
-        public static extern short Enumerate(short handle,
-                                              enPL1000Inputs channel,
-                                              out ushort value);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetValues")]
-        public static extern short GetValue(short handle,
-                                            short[] value,
-                                            ref ushort noOfValues,
-                                            out ushort overflow,
-                                            out uint triggerIndex);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000MaxValue")]
-        public static extern short MaxValue(short handle,
-                                            out ushort maxvalue);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnitAsync")]
-        public static extern short OpenUnitAsync(out short handle);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnitProgress")]
-        public static extern short pl1000OpenUnitProgress(out short handle,
-                                                            out short progress,
-                                                            out short complete);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Ready")]
-        public static extern short pl1000Ready(short handle,
-                                                out short ready);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Run")]
-        public static extern short Run(short handle,
-                                        uint NoOfValues,
-                                        enPL1000Method method);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetDo")]
-        public static extern short SetDo(short handle,
-                                            short do_value,
-                                            short DOchannel);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetInterval")]
-        public static extern short SetInterval(short handle,
-                                                ref uint usforblock,
-                                                uint IdealNoOfSamples,
-                                                short[] channels,
-                                                short NoofChannels);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetTrigger")]
-        public static extern short SetTrigger(short handle,
-                                              ushort enabled,
-                                              ushort autoTrigger,
-                                              ushort autoMS,
-                                              enPL1000Inputs channel,
-                                              ushort dir,
-                                              ushort threshold,
-                                              ushort hysteresis,
-                                              float delay);
-
-        [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Stop")]
-        public static extern short stop(short handle);
-
-        #endregion
+      PICO_OK = 0,
+      PICO_MAX_UNITS_OPENED = 1,
+      PICO_MEMORY_FAIL = 2,
+      PICO_NOT_FOUND = 3,
+      PICO_FW_FAIL = 4,
+      PICO_OPEN_OPERATION_IN_PROGRESS = 5,
+      PICO_OPERATION_FAILED = 6,
+      PICO_NOT_RESPONDING = 7,
+      PICO_CONFIG_FAIL = 8,
+      PICO_KERNEL_DRIVER_TOO_OLD = 9,
+      PICO_EEPROM_CORRUPT = 0x0A,
+      PICO_OS_NOT_SUPPORTED = 0x0B,
+      PICO_INVALID_HANDLE = 0x0C,
+      PICO_INVALID_PARAMETER = 0x0D,
+      PICO_INVALID_TIMEBASE = 0x0E,
+      PICO_INVALID_VOLTAGE_RANGE = 0x0F,
+      PICO_INVALID_CHANNEL = 0x10,
+      PICO_INVALID_TRIGGER_CHANNEL = 0x11,
+      PICO_INVALID_CONDITION_CHANNEL = 0x12,
+      PICO_NO_SIGNAL_GENERATOR = 0x13,
+      PICO_STREAMING_FAILED = 0x14,
+      PICO_BLOCK_MODE_FAILED = 0x15,
+      PICO_NULL_PARAMETER = 0x16,
+      PICO_DATA_NOT_AVAILABLE = 0x18,
+      PICO_STRING_BUFFER_TOO_SMALL = 0x19,
+      PICO_ETS_NOT_SUPPORTED = 0x1A,
+      PICO_AUTO_TRIGGER_TIME_TOO_SHORT = 0x1B,
+      PICO_BUFFER_STALL = 0x1C,
+      PICO_TOO_MANY_SAMPLES = 0x1D,
+      PICO_TOO_MANY_SEGMENTS = 0x1E,
+      PICO_PULSE_WIDTH_QUALIFIER = 0x1F,
+      PICO_DELAY = 0x20,
+      PICO_SOURCE_DETAILS = 0x21,
+      PICO_CONDITIONS = 0x22,
+      PICO_DEVICE_SAMPLING = 0x24,
+      PICO_NO_SAMPLES_AVAILABLE = 0x25,
+      PICO_SEGMENT_OUT_OF_RANGE = 0x26,
+      PICO_BUSY = 0x27,
+      PICO_STARTINDEX_INVALID = 0x28,
+      PICO_INVALID_INFO = 0x29,
+      PICO_INFO_UNAVAILABLE = 0x2A,
+      PICO_INVALID_SAMPLE_INTERVAL = 0x2B,
+      PICO_TRIGGER_ERROR = 0x2C,
+      PICO_MEMORY = 0x2D,
+      PICO_DELAY_NULL = 0x36,
+      PICO_INVALID_BUFFER = 0x37,
+      PICO_CANCELLED = 0x3A,
+      PICO_SEGMENT_NOT_USED = 0x3B,
+      PICO_NOT_USED = 0x3F,
+      PICO_INVALID_STATE = 0x41,
+      PICO_DRIVE_FUNCTION = 0x43
     }
+
+    public enum enPL1000Inputs
+    {
+      PL1000_CHANNEL_1 = 1,
+      PL1000_CHANNEL_2,
+      PL1000_CHANNEL_3,
+      PL1000_CHANNEL_4,
+      PL1000_CHANNEL_5,
+      PL1000_CHANNEL_6,
+      PL1000_CHANNEL_7,
+      PL1000_CHANNEL_8,
+      PL1000_CHANNEL_9,
+      PL1000_CHANNEL_10,
+      PL1000_CHANNEL_11,
+      PL1000_CHANNEL_12,
+      PL1000_CHANNEL_13,
+      PL1000_CHANNEL_14,
+      PL1000_CHANNEL_15,
+      PL1000_CHANNEL_16,
+      PL1000_MAX_CHANNELS = PL1000_CHANNEL_16
+    }
+
+    public enum enPL1000DO_Channel
+    {
+      PL1000_DO_CHANNEL_0,
+      PL1000_DO_CHANNEL_1,
+      PL1000_DO_CHANNEL_2,
+      PL1000_DO_CHANNEL_3,
+      PL1000_DO_CHANNEL_MAX
+    }
+
+    public enum enPL1000OpenProgress
+    {
+      PL1000_OPEN_PROGRESS_FAIL = -1,
+      PL1000_OPEN_PROGRESS_PENDING = 0,
+      PL1000_OPEN_PROGRESS_COMPLETE = 1
+    }
+
+    public enum enPL1000Method
+    {
+      SINGLE,
+      WINDOW,
+      STREAM,
+      MAX_METHOD = STREAM
+    }
+
+    #endregion
+
+    #region Driver Imports
+
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000CloseUnit")]
+    public static extern PicoErrorCode CloseUnit(short handle);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetUnitInfo")]
+    public static extern PicoErrorCode GetUnitInfo(short handle,
+                                          StringBuilder infoString,
+                                          short strlength,
+                                          out short reqSize,
+                                          int info);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnit")]
+    public static extern PicoErrorCode OpenUnit(out short handle);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetSingle")]
+    public static extern PicoErrorCode Enumerate(short handle,
+                                          enPL1000Inputs channel,
+                                          out ushort value);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000GetValues")]
+    public static extern PicoErrorCode GetValue(short handle,
+                                        ushort[] value,
+                                        ref uint noOfValues,
+                                        out ushort overflow,
+                                        out uint triggerIndex);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000MaxValue")]
+    public static extern PicoErrorCode MaxValue(short handle,
+                                        out ushort maxvalue);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnitAsync")]
+    public static extern PicoErrorCode OpenUnitAsync(out short handle);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000OpenUnitProgress")]
+    public static extern PicoErrorCode pl1000OpenUnitProgress(out short handle,
+                                                        out short progress,
+                                                        out short complete);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Ready")]
+    public static extern PicoErrorCode pl1000Ready(short handle,
+                                            out short ready);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Run")]
+    public static extern PicoErrorCode Run(short handle,
+                                    uint NoOfValues,
+                                    enPL1000Method method);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetDo")]
+    public static extern PicoErrorCode SetDo(short handle,
+                                        short do_value,
+                                        short DOchannel);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetInterval")]
+    public static extern PicoErrorCode SetInterval(short handle,
+                                            ref uint usforblock,
+                                            uint IdealNoOfSamples,
+                                            short[] channels,
+                                            short NoofChannels);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000SetTrigger")]
+    public static extern PicoErrorCode SetTrigger(short handle,
+                                          ushort enabled,
+                                          ushort autoTrigger,
+                                          ushort autoMS,
+                                          enPL1000Inputs channel,
+                                          ushort dir,
+                                          ushort threshold,
+                                          ushort hysteresis,
+                                          float delay);
+
+    [DllImport(_DRIVER_FILENAME, EntryPoint = "pl1000Stop")]
+    public static extern PicoErrorCode stop(short handle);
+
+    #endregion
+  }
 }
